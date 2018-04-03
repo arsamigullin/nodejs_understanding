7a665052e077016df2e3b8ea143b4a776454f2c5
arsamigullin <ar.smglln@gmail.com>
2018-04-03 21:27:14 +0300

    changed server.js

--- server.js
+++ server.js
@@ -1,20 +1,66 @@
-var http = require('http');
-var host = 'localhost';
-var port = '8080';
-var server = http.createServer((req, res)=> {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello world '+dowork());
+var http = require("http");
+const host = "localhost";
+const port = "8081";
+var server = http.createServer((req, res) => {
+  var c = new thisInTheReturnObject();
+  console.log(c.a());
+  thisInsideFunction(4,5);
+  thisInTheArrowedFunction();
+  thisInTheNestedObject();
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello world");
+});
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
 });
 
-function dowork(){
-    let b = 0;
-    for (let i=0;i<10000000;i++){
-        b++;
-    }
-    return b;
+function thisInsideFunction(id, num) {
+  this.id = id;
+  this.num = num;
+  this.getTotalNum = function() {
+    return this.id + " " + this.num;
+  };
 }
 
-server.listen(port, host, ()=>{
-    console.log(`Server is running at http://${host}:${port}`);
-});
+function thisInTheArrowedFunction() {
+  var globalObject = this;
+  var foo = () => this;
+  console.log(foo() === globalObject);
+  var obj = { foo: foo };
+  console.log(foo.call(obj) == globalObject);
+  foo = foo.bind(obj);
+  console.log(foo() == globalObject);
+}
+
+function thisInTheReturnObject() {
+  this.d = 37;
+  return {
+    a: function() {
+      return this.b;
+    },
+    b : this.d
+  };
+}
+
+function thisInTheNestedObject() {
+  var o = { prop: 37 };
+  function independent() {
+    return this.prop;
+  }
+  o.f = independent;
+  console.log(o.f()); // 37
+
+  o.b = { g: independent, prop: 42 };
+  console.log(o.b.g()); // logs 42
+
+  var globalObject = this;
+  var b = {
+    bar: function() {
+      return this;
+    }
+  };
+  var f = b.bar();
+  console.log(f() == globalObject);
+}
+
