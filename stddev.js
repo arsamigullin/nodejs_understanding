e9eda7e6bf973ba84405d3c8e5c6ad8c66778040
arsamigullin <ar.smglln@gmail.com>
2018-05-17 23:20:07 +0300

    std dev link

--- stddev.js
+++ stddev.js
@@ -1,4 +1,5 @@
 module.exports = {
+    //https://www.khanacademy.org/math/probability/data-distributions-a1/summarizing-spread-distributions/a/calculating-standard-deviation-step-by-step
     stdDev: function (data){
         let sum = 0 
         for (let i = 0; i<data.length; i++){
